25b8682f54561b3d7cb0fdce42a6a0d535f012e3
revrulb <revrulb@gmail.com>
2020-12-06 21:46:41 +0300

    removed unused import

--- Tests/utils.test.ts
+++ Tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { serverFaker, stringUtils } from "../Sources/Utils";
+import { stringUtils } from "../Sources/Utils";
 
 test("string utils tests", () => {
     const emptyString = "";
@@ -9,9 +9,4 @@ test("string utils tests", () => {
     expect(stringUtils.includes(stringA, emptyString)).toBe(true);
     expect(stringUtils.includes(stringAAA, stringA)).toBe(true);
     expect(stringUtils.includes(stringAAA, stringB)).toBe(false);
-});
-
-test("server faker tests", async () => {
-    const fakedServerData = await serverFaker.fakeRequest(5, 0);
-    expect(fakedServerData).toBe(5);
 });
